e822193ca3a5a3efa3d2498904468b9e50c536dc
Tye0426 <1076341064@qq.com>
2026-09-18 23:01:24 +0800

    Delete 第三版rail-shunting-h5 directory

--- 第三版rail-shunting-h5/solver-worker.js
+++
@@ -1,5 +0,0 @@
-importScripts('./solver.js');
-self.onmessage = event => {
-  try { self.postMessage({ type: 'result', result: ShuntingSolver.solve(event.data, stats => self.postMessage({ type: 'progress', stats })) }); }
-  catch (error) { self.postMessage({ type: 'error', message: error.message }); }
-};
